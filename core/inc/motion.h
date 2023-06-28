2d11cd733049014b56e5d7c124365f8d46453b1e
BusyBox <2894415997@qq.com>
2023-06-28 13:00:08 +0800

    二维平面上下左右移动

--- core/inc/motion.h
+++ core/inc/motion.h
@@ -3,74 +3,69 @@
 
 #include "motor.h"
 
-
-
 #ifdef __cplusplus
 
+#include <utility>
+#include <cmath>
 
 namespace RDK
 {
 
+    /*
+     * @brief 4个麦克纳姆轮(ABBA)
+     *
+     *  |-----------------------|
+     *  | motor1(A)   motor2(B) |
+     *  |                       |
+     *  |                       |
+     *  |                       |
+     *  |                       |
+     *  |                       |
+     *  |                       |
+     *  |                       |
+     *  |                       |
+     *  | motor3(B)   motor4(A) |
+     *  |-----------------------|
+     *
+     *  小车前方为x轴，左边为y轴
+     */
+    class MecanumMotion
+    {
+    private:
+        EncodingMotor *motor1 = nullptr;
+        EncodingMotor *motor2 = nullptr;
+        EncodingMotor *motor3 = nullptr;
+        EncodingMotor *motor4 = nullptr;
+        double motor1Speed = 0;
+        double motor2Speed = 0;
+        double motor3Speed = 0;
+        double motor4Speed = 0;
+        double kp = 0.01;
+        double ki = 0.1;
+        double kd = 0;
+        double minTotalError = -10;
+        double maxTotalError = 10;
+        std::pair<double, double> wheelADirectionVector = {1, -1};
+        std::pair<double, double> wheelBDirectionVector = {1, 1};
 
-/*
- * @brief 4个麦克纳姆轮(ABBA)
- * 
- *  |-----------------------|
- *  | motor1(A)   motor2(B) |
- *  |                       | 
- *  |                       |
- *  |                       | 
- *  |                       |
- *  |                       | 
- *  |                       |
- *  |                       | 
- *  |                       |
- *  | motor3(B)   motor4(A) |
- *  |-----------------------|    
- *           
- *  小车前方为x轴，左边为y轴
- */
-class MecanumMotion
-{
-private:
-    EncodingMotor* motor1 = nullptr;
-    EncodingMotor* motor2 = nullptr;
-    EncodingMotor* motor3 = nullptr;
-    EncodingMotor* motor4 = nullptr;
-    double motor1Speed = 0;
-    double motor2Speed = 0;
-    double motor3Speed = 0;
-    double motor4Speed = 0;
-    double kp = 0;
-    double ki = 0;
-    double kd = 0;
-public:
-    MecanumMotion();
-    ~MecanumMotion();
-    void SetEncodingMotor1(EncodingMotor* motor);
-    void SetEncodingMotor2(EncodingMotor* motor);
-    void SetEncodingMotor3(EncodingMotor* motor);
-    void SetEncodingMotor4(EncodingMotor* motor);
-    void ClearSpeed();
-    void AddXSpeed(double speed);
-    void AddYSpeed(double speed);
-    void AddZSpeed(double speed);
-    void CommitSpeed();
-    void SetPosPID(double p, double i, double d);
-    void Move(double speed, double xDis, double yDis);
-};
-
+    public:
+        MecanumMotion();
+        ~MecanumMotion();
+        void SetEncodingMotor1(EncodingMotor *motor);
+        void SetEncodingMotor2(EncodingMotor *motor);
+        void SetEncodingMotor3(EncodingMotor *motor);
+        void SetEncodingMotor4(EncodingMotor *motor);
+        void ClearSpeed();
+        void AddXSpeed(double speed);
+        void AddYSpeed(double speed);
+        void AddZSpeed(double speed);
+        void CommitSpeed();
+        void SetPosPID(double p, double i, double d);
+        void Move(double speed, double xDis, double yDis);
+    };
 
 }
 
-
 #endif
-
-
-
-
-
-
-
 
 #endif
--- core/src/motion.cpp
+++ core/src/motion.cpp
@@ -2,98 +2,155 @@
 
 namespace RDK
 {
-	
 
-MecanumMotion::MecanumMotion()
-{
+	MecanumMotion::MecanumMotion()
+	{
+		double wheelDirectionVectorLen = std::sqrt(2);
+		wheelADirectionVector.first /= wheelDirectionVectorLen;
+		wheelADirectionVector.second /= wheelDirectionVectorLen;
+		wheelBDirectionVector.first /= wheelDirectionVectorLen;
+		wheelBDirectionVector.second /= wheelDirectionVectorLen;
+	}
 
-}
+	MecanumMotion::~MecanumMotion()
+	{
+	}
 
-MecanumMotion::~MecanumMotion()
-{
+	void MecanumMotion::SetEncodingMotor1(EncodingMotor *motor)
+	{
+		this->motor1 = motor;
+	}
 
-}
+	void MecanumMotion::SetEncodingMotor2(EncodingMotor *motor)
+	{
+		this->motor2 = motor;
+	}
 
-void MecanumMotion::SetEncodingMotor1(EncodingMotor* motor)
-{
-	this->motor1 = motor;
-}
+	void MecanumMotion::SetEncodingMotor3(EncodingMotor *motor)
+	{
+		this->motor3 = motor;
+	}
 
-void MecanumMotion::SetEncodingMotor2(EncodingMotor* motor)
-{
-	this->motor2 = motor;
-}
+	void MecanumMotion::SetEncodingMotor4(EncodingMotor *motor)
+	{
+		this->motor4 = motor;
+	}
 
-void MecanumMotion::SetEncodingMotor3(EncodingMotor* motor)
-{
-	this->motor3 = motor;
-}
+	void MecanumMotion::ClearSpeed()
+	{
+		motor1Speed = 0;
+		motor2Speed = 0;
+		motor3Speed = 0;
+		motor4Speed = 0;
+	}
 
-void MecanumMotion::SetEncodingMotor4(EncodingMotor* motor)
-{
-	this->motor4 = motor;
-}
+	void MecanumMotion::AddXSpeed(double speed)
+	{
+		motor1Speed += speed;
+		motor2Speed += speed;
+		motor3Speed += speed;
+		motor4Speed += speed;
+	}
 
-void MecanumMotion::ClearSpeed()
-{
-	motor1Speed = 0;
-	motor2Speed = 0;
-	motor3Speed = 0;
-	motor4Speed = 0;
-}
+	void MecanumMotion::AddYSpeed(double speed)
+	{
+		motor1Speed -= speed;
+		motor2Speed += speed;
+		motor3Speed += speed;
+		motor4Speed -= speed;
+	}
 
-void MecanumMotion::AddXSpeed(double speed)
-{
-	motor1Speed += speed;
-	motor2Speed += speed;
-	motor3Speed += speed;
-	motor4Speed += speed;
-}
+	void MecanumMotion::AddZSpeed(double speed)
+	{
+		motor1Speed -= speed;
+		motor2Speed += speed;
+		motor3Speed -= speed;
+		motor4Speed += speed;
+	}
 
-void MecanumMotion::AddYSpeed(double speed)
-{
-	motor1Speed -= speed;
-	motor2Speed += speed;
-	motor3Speed += speed;
-	motor4Speed -= speed;
-}
+	void MecanumMotion::CommitSpeed()
+	{
+		if (motor1)
+			motor1->SetSpeed(motor1Speed);
+		if (motor2)
+			motor2->SetSpeed(motor2Speed);
+		if (motor3)
+			motor3->SetSpeed(motor3Speed);
+		if (motor4)
+			motor4->SetSpeed(motor4Speed);
+	}
 
-void MecanumMotion::AddZSpeed(double speed)
-{
-	motor1Speed -= speed;
-	motor2Speed += speed;
-	motor3Speed -= speed;
-	motor4Speed += speed;
-}
+	void MecanumMotion::SetPosPID(double p, double i, double d)
+	{
+		this->kp = p;
+		this->ki = i;
+		this->kd = d;
+	}
 
-void MecanumMotion::CommitSpeed()
-{
-	if (motor1) motor1->SetSpeed(motor1Speed);
-	if (motor2) motor2->SetSpeed(motor2Speed);
-	if (motor3) motor3->SetSpeed(motor3Speed);
-	if (motor4) motor4->SetSpeed(motor4Speed);
-}
+	/*
+	 * @brief 2维平面内上下左右移动
+	 * @attention 调用此方法会一直阻塞到移动结束！
+	 * @param speed 移动速度
+	 * @param xDis x轴移动距离
+	 * @param yDis y轴移动距离
+	 */
+	void MecanumMotion::Move(double speed, double xDis, double yDis)
+	{
+		PID motor1PosPID(RDK::PIDType::Pos, this->kp, this->ki, this->kd);
+		PID motor2PosPID(RDK::PIDType::Pos, this->kp, this->ki, this->kd);
+		PID motor3PosPID(RDK::PIDType::Pos, this->kp, this->ki, this->kd);
+		PID motor4PosPID(RDK::PIDType::Pos, this->kp, this->ki, this->kd);
+		if (speed < 0)
+			speed = -speed;
+		motor1PosPID.SetTotalErrorRange(minTotalError, maxTotalError);
+		motor2PosPID.SetTotalErrorRange(minTotalError, maxTotalError);
+		motor3PosPID.SetTotalErrorRange(minTotalError, maxTotalError);
+		motor4PosPID.SetTotalErrorRange(minTotalError, maxTotalError);
+		motor1PosPID.SetOutputRange(-speed, speed);
+		motor2PosPID.SetOutputRange(-speed, speed);
+		motor3PosPID.SetOutputRange(-speed, speed);
+		motor4PosPID.SetOutputRange(-speed, speed);
 
-void MecanumMotion::SetPosPID(double p, double i, double d)
-{
-	this->kp = p;
-	this->ki = i;
-	this->kd = d;
-}
+		double motor1TargetDis = xDis * wheelADirectionVector.first + yDis * wheelADirectionVector.second;
+		double motor2TargetDis = xDis * wheelBDirectionVector.first + yDis * wheelBDirectionVector.second;
+		double motor3TargetDis = xDis * wheelBDirectionVector.first + yDis * wheelBDirectionVector.second;
+		double motor4TargetDis = xDis * wheelADirectionVector.first + yDis * wheelADirectionVector.second;
 
-/*
- * @brief 2维平面内上下左右移动
- * @attention 调用此方法会一直阻塞到移动结束！
- * @param speed 移动速度
- * @param xDis x轴移动距离
- * @param yDis y轴移动距离
- */
-void MecanumMotion::Move(double speed, double xDis, double yDis)
-{
-	
-}
+		motor1->ClearPulse();
+		motor2->ClearPulse();
+		motor3->ClearPulse();
+		motor4->ClearPulse();
 
+		motor1PosPID.SetTarget(motor1TargetDis);
+		motor2PosPID.SetTarget(motor2TargetDis);
+		motor3PosPID.SetTarget(motor3TargetDis);
+		motor4PosPID.SetTarget(motor4TargetDis);
 
+		while (true)
+		{
+			if (std::fabs(motor1->GetPulse() - motor1TargetDis) < 5 &&
+				std::fabs(motor2->GetPulse() - motor2TargetDis) < 5 &&
+				std::fabs(motor3->GetPulse() - motor3TargetDis) < 5 &&
+				std::fabs(motor4->GetPulse() - motor4TargetDis) < 5)
+				break;
+			motor1PosPID.SetInput(motor1->GetPulse());
+			motor1PosPID.Tick();
+			motor2PosPID.SetInput(motor2->GetPulse());
+			motor2PosPID.Tick();
+			motor3PosPID.SetInput(motor3->GetPulse());
+			motor3PosPID.Tick();
+			motor4PosPID.SetInput(motor4->GetPulse());
+			motor4PosPID.Tick();
 
+			ClearSpeed();
+			motor1Speed += motor1PosPID.GetOutput();
+			motor2Speed += motor2PosPID.GetOutput();
+			motor3Speed += motor3PosPID.GetOutput();
+			motor4Speed += motor4PosPID.GetOutput();
+			CommitSpeed();
+		}
+		ClearSpeed();
+		CommitSpeed();
+	}
 
 }
