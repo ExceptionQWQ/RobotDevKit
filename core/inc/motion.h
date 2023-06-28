b6806b35bdf458dd1b441c877bfec2555212bd2e
BusyBox <2894415997@qq.com>
2023-06-28 10:55:35 +0800

    麦克纳姆轮速度控制

--- core/inc/motion.h
+++ core/inc/motion.h
@@ -1,10 +1,71 @@
 #ifndef MOTION_H
 #define MOTION_H
 
+#include "motor.h"
 
-#include <iostream>
-#include "pid.h"
 
-void hello();
+
+#ifdef __cplusplus
+
+
+namespace RDK
+{
+
+
+/*
+ * @brief 4个麦克纳姆轮(ABBA)
+ * 
+ *  |-----------------------|
+ *  | motor1(A)   motor2(B) |
+ *  |                       | 
+ *  |                       |
+ *  |                       | 
+ *  |                       |
+ *  |                       | 
+ *  |                       |
+ *  |                       | 
+ *  |                       |
+ *  | motor3(B)   motor4(A) |
+ *  |-----------------------|    
+ *           
+ *  小车前方为x轴，左边为y轴
+ */
+class MecanumMotion
+{
+private:
+    EncodingMotor* motor1 = nullptr;
+    EncodingMotor* motor2 = nullptr;
+    EncodingMotor* motor3 = nullptr;
+    EncodingMotor* motor4 = nullptr;
+    double motor1Speed = 0;
+    double motor2Speed = 0;
+    double motor3Speed = 0;
+    double motor4Speed = 0;
+public:
+    MecanumMotion();
+    ~MecanumMotion();
+    void SetEncodingMotor1(EncodingMotor* motor);
+    void SetEncodingMotor2(EncodingMotor* motor);
+    void SetEncodingMotor3(EncodingMotor* motor);
+    void SetEncodingMotor4(EncodingMotor* motor);
+    void ClearSpeed();
+    void AddXSpeed(double speed);
+    void AddYSpeed(double speed);
+    void AddZSpeed(double speed);
+    void CommitSpeed();
+};
+
+
+}
+
+
+#endif
+
+
+
+
+
+
+
 
 #endif
--- core/src/motion.cpp
+++ core/src/motion.cpp
@@ -1,6 +1,82 @@
 #include "motion.h"
 
-void hello()
+namespace RDK
 {
-	std::cout << "hello" << std::endl;
+	
+
+MecanumMotion::MecanumMotion()
+{
+
+}
+
+MecanumMotion::~MecanumMotion()
+{
+
+}
+
+void MecanumMotion::SetEncodingMotor1(EncodingMotor* motor)
+{
+	this->motor1 = motor;
+}
+
+void MecanumMotion::SetEncodingMotor2(EncodingMotor* motor)
+{
+	this->motor2 = motor;
+}
+
+void MecanumMotion::SetEncodingMotor3(EncodingMotor* motor)
+{
+	this->motor3 = motor;
+}
+
+void MecanumMotion::SetEncodingMotor4(EncodingMotor* motor)
+{
+	this->motor4 = motor;
+}
+
+void MecanumMotion::ClearSpeed()
+{
+	motor1Speed = 0;
+	motor2Speed = 0;
+	motor3Speed = 0;
+	motor4Speed = 0;
+}
+
+void MecanumMotion::AddXSpeed(double speed)
+{
+	motor1Speed += speed;
+	motor2Speed += speed;
+	motor3Speed += speed;
+	motor4Speed += speed;
+}
+
+void MecanumMotion::AddYSpeed(double speed)
+{
+	motor1Speed -= speed;
+	motor2Speed += speed;
+	motor3Speed += speed;
+	motor4Speed -= speed;
+}
+
+void MecanumMotion::AddZSpeed(double speed)
+{
+	motor1Speed -= speed;
+	motor2Speed += speed;
+	motor3Speed -= speed;
+	motor4Speed += speed;
+}
+
+void MecanumMotion::CommitSpeed()
+{
+	if (motor1) motor1->SetSpeed(motor1Speed);
+	if (motor2) motor2->SetSpeed(motor2Speed);
+	if (motor3) motor3->SetSpeed(motor3Speed);
+	if (motor4) motor4->SetSpeed(motor4Speed);
+}
+
+
+
+
+
+
 }
