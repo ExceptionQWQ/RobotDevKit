f8c61444152ec26a55182486730cc7e49b095947
BusyBox <2894415997@qq.com>
2024-06-30 21:50:57 +0800

    新增DR16接收器协议

---
+++ platforms/stm32f4/rdk/core/remote/dr16.h
@@ -0,0 +1,111 @@
+/*
+ * @author BusyBox
+ * @date 2024/6/30
+ * @version 1.0
+ * @git https://github.com/ExceptionQWQ/RobotDevKit
+ */
+
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+#include <memory>
+#include "rdk/core/transfer/serial_port.h"
+
+/*
+ * DR16接收器
+ * CubeMX中配置串口时需注意 Baud Rate为100000，Word Length为9bits，Parity为Even，Stop Bits为1
+ *
+ */
+
+class DR16
+{
+public:
+    DR16(std::shared_ptr<SerialPort> serialPort);
+    ~DR16();
+
+    /*
+     * @brief 启动接收遥控器函数
+     * @attention 请单独放在一个线程中，该函数为阻塞函数，调用后不会返回
+     */
+    void start();
+
+    /*
+     * @brief 获取通道0的数据
+     * @details 最小值 364 中间值 1024 最大值 1684
+     */
+    uint16_t get_channel_0();
+
+    /*
+     * @brief 获取通道1的数据
+     * @details 最小值 364 中间值 1024 最大值 1684
+     */
+    uint16_t get_channel_1();
+
+    /*
+     * @brief 获取通道2的数据
+     * @details 最小值 364 中间值 1024 最大值 1684
+     */
+    uint16_t get_channel_2();
+
+    /*
+     * @brief 获取通道3的数据
+     * @details 最小值 364 中间值 1024 最大值 1684
+     */
+    uint16_t get_channel_3();
+
+    /*
+     * @brief 获取s1的数据
+     * @details 最小值 1 最大值 3
+     */
+    uint8_t get_s1();
+
+    /*
+     * @brief 获取s2的数据
+     * @details 最小值 1 最大值 3
+     */
+    uint8_t get_s2();
+
+
+private:
+    std::shared_ptr<SerialPort> serialPort;
+
+#pragma pack(push, 1)
+    union DBUS_Data_Def
+    {
+        struct {
+            uint16_t channel_0:11;
+            uint16_t channel_1:11;
+            uint16_t channel_2:11;
+            uint16_t channel_3:11;
+            uint8_t s1:2;
+            uint8_t s2:2;
+            uint16_t mouse_x:16;
+            uint16_t mouse_y:16;
+            uint16_t mouse_z:16;
+            uint8_t mouse_left:8;
+            uint8_t mouse_right:8;
+            uint16_t key:16;
+        };
+        uint8_t data[18];
+        uint8_t recv_buff[64];
+    }dbus_data;
+#pragma pack(pop)
+
+    struct RemoteData
+    {
+        uint16_t channel_0;
+        uint16_t channel_1;
+        uint16_t channel_2;
+        uint16_t channel_3;
+        uint8_t s1;
+        uint8_t s2;
+        uint16_t mouse_x;
+        uint16_t mouse_y;
+        uint16_t mouse_z;
+        uint8_t mouse_left;
+        uint8_t mouse_right;
+    }remote_data;
+
+};
+
